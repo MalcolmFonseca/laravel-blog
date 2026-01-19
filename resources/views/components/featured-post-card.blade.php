30593254a0b968d525db6c2c58eccc16a3587ce1
MalcolmFonseca <malcolmfonseca@hotmail.com>
2026-01-19 14:29:13 -0500

    fixed featured post card to match other card adjustments

--- resources/views/components/featured-post-card.blade.php
+++ resources/views/components/featured-post-card.blade.php
@@ -7,10 +7,14 @@
         <img src="https://placehold.net/7-800x600.png" width="300" height="200" />
     @endif
     <div class="FeaturedPostInfo">
+        <p class="PostCategory"><a
+                href="/blog/?category=<?= $post->category->slug ?>"><?= strtoupper($post->category->name) ?></a></p>
         <h2 class="PostTitle"><a href="/posts/<?= $post->slug ?>"><?= $post->title ?></a></h2>
-        <p><a href="/blog/?user=<?= $post->user->username ?>"><?= 'By: ' . $post->user->name ?></a></p>
-        <p><?= $post->created_at->diffForHumans() ?></p>
-        <p><a href="/blog/?category=<?= $post->category->slug ?>"><?= ucfirst($post->category->name) ?></a></p>
-        <div><?= $post->excerpt ?></div>
+        <p><?= $post->excerpt ?></p>
+        <hr>
+        <div class="PostInfoFooter">
+            <p><a href="/blog/?user=<?= $post->user->username ?>"><?= 'By: ' . $post->user->name ?></a></p>
+            <p><?= $post->created_at->diffForHumans() ?></p>
+        </div>
     </div>
 </li>
